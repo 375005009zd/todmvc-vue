f418a250290c6994e7c6a1dc2cb7ea86149bd069
375005009zd <375005009zd@github.com>
2019-05-29 20:35:34 +0800

    init

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 (function (window) {
-	'use strict';
-
-	// Your starting point. Enjoy the ride!
-
+	var todos = [
+		{
+			id: 0,
+			title: '吃饭',
+			completed: true
+		},
+		{
+			id: 1,
+			title: '睡觉',
+			completed: false
+		},
+		{
+			id: 2,
+			title: '游戏',
+			completed: false
+		},
+	];
+	new Vue({
+		data:{
+			todos
+		}
+		
+	}).$mount('#app');
 })(window);

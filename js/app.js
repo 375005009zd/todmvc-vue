1bf2d8f06eb92046c012fa942c80450a412ebca7
375005009zd <375005009zd@github.com>
2019-05-30 17:46:13 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,24 +1,78 @@
+
 (function (window) {
 	var todos = [
-		{
-			id: 0,
-			title: '吃饭',
-			completed: true
-		},
-		{
-			id: 1,
-			title: '睡觉',
-			completed: false
-		},
-		{
-			id: 2,
-			title: '游戏',
-			completed: false
-		},
+		// {
+		// 	id: 0,
+		// 	title: '吃饭',
+		// 	completed: true
+		// },
+		// {
+		// 	id: 1,
+		// 	title: '睡觉',
+		// 	completed: false
+		// },
+		// {
+		// 	id: 2,
+		// 	title: '游戏',
+		// 	completed: false
+		// },
 	];
-	new Vue({
+var app = new Vue({
 		data:{
-			todos
+			todos,
+			currentEiditing:''
+		},
+		computed:{
+			remainCount(){
+				return this.todos.filter(item => !item.completed).length
+			}
+
+		},
+		methods:{
+			handleAddTodo(e){
+			   var target = e.target;
+			   var value = target.value.trim();
+			   if(value){
+				   this.todos.push({
+					   id:this.todos.length,
+					   title: value,
+					   completed:false
+				   })
+			   }
+			   target.value = ""
+			},
+			handleToggleAll(e){
+				const checked = e.target.checked;
+				this.todos.forEach(item => {
+					item.completed = checked
+				})
+
+			},
+			handleRemoveTodo(index){
+              this.todos.splice(index,1)
+			},
+			getEditingHandle(id){
+                this.currentEiditing = id;
+			},
+			handleEditTodo( item,index,e ){
+				const target = e.target;
+				var value = target.value;
+				if(!value){
+                    this.todos.splice(index,1)
+				}else{
+					item.title = value
+				}
+				this.currentEiditing = null
+			},
+			handleClearCompleteTodo() {
+			   for(var i=0;i<this.todos.length;i++){
+				   var item = this.todos[i]
+                  if(item.completed){
+					   this.todos.splice(i,1)
+					   i--
+				  }
+			   }
+			}
 		}
 		
 	}).$mount('#app');
